11726f08f0efa46add5c57b04367a2c276af4d97
Edoardo Miglioli <edoardomiglioli2008@gmail.com>
2024-03-26 08:36:14 +0100

    made the current step style change

--- multi-step-form/src/components/Sidebar/Sidebar.jsx
+++ multi-step-form/src/components/Sidebar/Sidebar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import StepListElement from "./StepListElement";
 
-const Sidebar = () => {
+const Sidebar = ({ currentStep }) => {
     return (
         <div className="sidebar">
-            <StepListElement stepNumber="1" stepAction="YOUR INFO" />
-            <StepListElement stepNumber="2" stepAction="SELECT PLAN" />
-            <StepListElement stepNumber="3" stepAction="ADD-ONS" />
-            <StepListElement stepNumber="4" stepAction="SUMMARY" />
+            <StepListElement stepNumber="1" stepAction="YOUR INFO" currentStep={currentStep} />
+            <StepListElement stepNumber="2" stepAction="SELECT PLAN" currentStep={currentStep} />
+            <StepListElement stepNumber="3" stepAction="ADD-ONS" currentStep={currentStep} />
+            <StepListElement stepNumber="4" stepAction="SUMMARY" currentStep={currentStep} />
         </div>
     );
 }
